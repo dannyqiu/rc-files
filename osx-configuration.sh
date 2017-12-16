424aa1899853e241134880d476e7c02f8f2917e0
Danny Qiu <dqiu55@gmail.com>
2017-12-15 23:45:02 -0500

    Add MAC OS X configuration

---
+++ osx-configuration.sh
@@ -0,0 +1,11 @@
+# Show Full Path in Finder Title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Disable Creation of Metadata Files on Network Volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Disable Creation of Metadata Files on USB Volumes
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Get SF Mono Fonts (OS X Sierra +)
+cp -v /Applications/Utilities/Terminal.app/Contents/Resources/Fonts/SFMono-* ~/Library/Fonts
