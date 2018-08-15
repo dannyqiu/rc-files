72c8a14dd02092d8b4dc1fce36944283d1e371ef
Danny Qiu <dqiu55@gmail.com>
2018-08-14 22:38:25 -0400

    Add zsh-syntax-highlighting plugin

--- install-zsh-plugins.sh
+++ install-zsh-plugins.sh
@@ -3,3 +3,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 echo "Installing zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo "Installing zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
