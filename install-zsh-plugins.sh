83484b7fab7a5f579789df4dadc1c0688e51bc28
Danny Qiu <dqiu55@gmail.com>
2023-12-06 19:06:02 -0800

    Update ohmyzsh install url

--- install-zsh-plugins.sh
+++ install-zsh-plugins.sh
@@ -1,5 +1,5 @@
 echo "Installing oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "Installing zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
