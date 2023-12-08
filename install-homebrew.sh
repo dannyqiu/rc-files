9cb4aba55ca5cda60b096e8422d512b00c10a125
Danny Qiu <dqiu55@gmail.com>
2023-12-08 01:31:35 -0800

    Add script for homebrew installation

---
+++ install-homebrew.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+echo "Installing Homebrew"
+brew -v || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+PACKAGES=(
+    binwalk
+    ffmpeg
+    htop
+    jq
+    openjdk
+    pyenv
+    the_silver_searcher
+    tmux
+    vim
+    vlc
+    wakeonlan
+    wget
+    wifi-password
+)
+CASK_PACKAGES=(
+    appcleaner
+    bit-slicer
+    db-browser-for-sqlite
+    handbrake
+    imageoptim
+    qlcolorcode
+    qlimagesize
+    qlmarkdown
+    qlstephen
+    rectangle
+    virtual-studio-code
+    wireshark
+    xquartz
+)
+
+echo "Installing packages: ${PACKAGES[@]}"
+brew install ${PACKAGES[@]}
+
+# Link OpenJDK into system Java wrappers
+sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
